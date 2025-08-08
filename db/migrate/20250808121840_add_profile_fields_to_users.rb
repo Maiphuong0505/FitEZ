c597e3e8f6a0c98c5d2927a2db39952a7ed34e3d
Bruno Vetorazo <vetorazo@gmail.com>
2025-08-08 21:20:55 +0900

    Added necessary columns to user table

---
+++ db/migrate/20250808121840_add_profile_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddProfileFieldsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :gender, :string
+    add_column :users, :date_of_birth, :date
+    add_column :users, :is_a_trainer, :boolean
+  end
+end
