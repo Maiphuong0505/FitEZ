4f98f3a4d0dee667ffa04189cff93e8d34ac324d
MaiPhuong NguyenThi <nguyenthimaiphuong2416@gmail.com>
2025-08-09 15:16:35 +0900

    Write the logic for workout plan seed file

--- db/seeds/workout_plans.rb
+++ db/seeds/workout_plans.rb
@@ -1,3 +1,25 @@
-puts "Clearing the database for workout plans"
+puts "Clearing the database for workout plans..."
 
 WorkoutPlan.destroy_all
+
+# Each workout plan will have a starting date, ending date, a trainer and a client
+
+trainer = User.where(is_a_trainer: true)
+clients = User.where(is_a_trainer: false)
+
+# Iterate over the clients array
+# For each of them we create a new instance of WorkoutPlan
+gaps = [30, 60, 90]
+
+clients.each do |client|
+  puts "Creating one workout plan for #{client.last_name} #{client.first_name}."
+  starting_date = Random.rand(Time.zone.tomorrow.last_year..Time.zone.tomorrow)
+  workout_plan = WorkoutPlan.new(
+    starting_date: starting_date,
+    ending_date: starting_date + gaps.sample,
+    trainer_id: trainer.first.id,
+    client_id: client.id
+  )
+  workout_plan.save
+  puts "Finished creating one workout plan for #{client.last_name} #{client.first_name}."
+end
