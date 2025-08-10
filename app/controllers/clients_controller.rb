15ba7a12cd20f49ef90a68751ac0d3f7b76ee8cd
MaiPhuong NguyenThi <nguyenthimaiphuong2416@gmail.com>
2025-08-10 21:24:36 +0900

    Created custom controller called clients, added routes to clients controller and the #index view. Deactivate pundit because it brokes devise Log_in feature. Divided seed files for users and body_stats. Fixed association errors between User model and BodyStat

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,15 @@
+class ClientsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = policy_scope(User)
+    # skip_authorization_check
+    @my_clients = User.my_clients(current_user.id)
+    # @my_clients = clients.select do |client|
+    #   trainer_ids = client.workout_plans_as_client.map(&:trainer_id)
+    #   trainer_ids.include?(current_user.id)
+    # end
+  end
+
+  def show
+  end
+end
