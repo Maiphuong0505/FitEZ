9d0f52f233c60d62a3f02e576c2c61bba30af867
MaiPhuong NguyenThi <nguyenthimaiphuong2416@gmail.com>
2025-08-12 13:22:33 +0900

    Testing seed file for workout-session

--- db/seeds/workout_sessions.rb
+++ db/seeds/workout_sessions.rb
@@ -7,13 +7,11 @@ muscles = %w[Upper-body Lower-body Full-body Push Pull Legs]
 
 workout_plans = WorkoutPlan.all
 
-
-
 workout_plans.each do |plan|
   WorkoutSession.create(
     workout_plan_id: plan.id,
     session_name: "#{muscles.sample} Focus",
-    date_and_time: Time.parse("#{plan.starting_date + 2} #{rand(10..22)}:00:00"),
+    date_time: Time.parse("#{plan.starting_date + 2} #{rand(10..22)}:00:00"),
     with_trainer: true
   )
 
