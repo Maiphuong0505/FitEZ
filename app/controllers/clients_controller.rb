161e5a887a654828c2591ec69079f6ba1365fd70
MaiPhuong NguyenThi <nguyenthimaiphuong2416@gmail.com>
2025-08-14 15:31:40 +0900

    Updated Pundit logic for each method but still failed. For now, accessibility to each page is controlled by user's status and link_to display

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,16 +2,12 @@ class ClientsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_client, only: [:show]
   def index
-    @users = policy_scope(User)
+    @clients = policy_scope(User)
     @my_clients = User.my_clients(current_user.id)
-    # @my_clients = clients.select do |client|
-    #   trainer_ids = client.workout_plans_as_client.map(&:trainer_id)
-    #   trainer_ids.include?(current_user.id)
-    # end
   end
 
   def show
-    # authorize @client
+    authorize @client
     @workout_plans = @client.workout_plans_as_client
     @body_stat = BodyStat.new
   end
