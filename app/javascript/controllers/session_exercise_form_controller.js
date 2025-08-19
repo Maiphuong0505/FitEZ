266844711970258d2ed37949405df01a04f79837
Bruno Vetorazo <vetorazo@gmail.com>
2025-08-19 20:16:09 +0900

    Added turbo stream to comment controller and put the comment section into a partial

--- app/javascript/controllers/session_exercise_form_controller.js
+++ app/javascript/controllers/session_exercise_form_controller.js
@@ -1,24 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import TomSelect from "tom-select";
 
 // Connects to data-controller="form-prevent-default"
 export default class extends Controller {
-  static targets = ["tomSelect"]
+  static targets = ["tomSelect"];
 
   reset() {
-    console.log("reset")
-    console.log(this.element)
-    this.element.reset()
-    this.tomSelect.clear()
+    console.log("reset");
+    console.log(this.element);
+    this.element.reset();
+    if (this.tomSelect) {
+      this.tomSelect.clear();
+    }
   }
 
   connect() {
-    this.tomSelect = new TomSelect(this.tomSelectTarget, {
-      create: true,
-      sortField: {
-        field: "text",
-        direction: "asc",
-      },
-    });
+    if (this.hasTomSelectTarget) {
+      this.tomSelect = new TomSelect(this.tomSelectTarget, {
+        create: true,
+        sortField: {
+          field: "text",
+          direction: "asc",
+        },
+      });
+    }
   }
 }
