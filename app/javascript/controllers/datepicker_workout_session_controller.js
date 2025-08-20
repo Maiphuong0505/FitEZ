bd286af120e974d91e776ca42fa7ad2911c5ae24
MaiPhuong NguyenThi <nguyenthimaiphuong2416@gmail.com>
2025-08-20 22:27:33 +0900

    Add flatpickr to workout session form

---
+++ app/javascript/controllers/datepicker_workout_session_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="datepicker-workout-session"
+export default class extends Controller {
+  connect() {
+    const now = new Date()
+    flatpickr(this.element, {
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+      minDate: "today",
+      time_24hr: true,
+      minTime: "7:00",
+      maxTime: "21:00",
+    })
+    console.log("shitake workout session")
+  }
+}
