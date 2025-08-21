e446bb200d62c16728f776a45bf0fe46b312da1d
Bruno Vetorazo <vetorazo@gmail.com>
2025-08-21 18:30:15 +0900

    Added some different logic, still not able to copy and somehow broke the session form

--- app/controllers/workout_sessions_controller.rb
+++ app/controllers/workout_sessions_controller.rb
@@ -1,5 +1,5 @@
 class WorkoutSessionsController < ApplicationController
-  before_action :set_workout_session, only: %i[show]
+  before_action :set_workout_session, only: %i[show copy]
   before_action :set_client, only: %i[create copy]
 
   def show
@@ -9,10 +9,26 @@ class WorkoutSessionsController < ApplicationController
     @comment = Comment.new
   end
 
+  # Method to copy existing session
+  def copy
+    @workout_session_copy = @workout_session.deep_clone include: :session_exercises
+    @workout_session_copy.session_name = "#{@workout_session.session_name} (Copy)"
+    @workout_session_copy.date_time = Time.current
+    @workout_session_copy.id = nil
+    authorize @workout_session_copy
+    render :new, locals: { workout_session: @workout_session_copy, copy_from_id: @workout_session.id }
+  end
+
   def create
     @workout_session = WorkoutSession.new(workout_session_params)
     authorize @workout_session
     if @workout_session.save
+      if params[:copy_from_id].present?
+        original = WorkoutSession.find(params[:copy_from_id])
+        original.session_exercises.each do |exercise|
+          @workout_session.session_exercises.create(exercise.attributes.except("id", "workout_session_id", "created_at", "updated_at"))
+        end
+      end
       redirect_to workout_session_path(@workout_session), notice: "Workout session created successfully."
     else
       # render client show page if workout plan is invalid
@@ -23,29 +39,8 @@ class WorkoutSessionsController < ApplicationController
     end
   end
 
-  # Method to copy existing session
-  def copy
-    @copied_session = copy_session(params[:id])
-    authorize @copied_session
-    if @copied_session.save
-      redirect_to workout_session_path(@copied_session), notice: "Session copied successfully"
-    else
-      @workout_plans = @client.workout_plans_as_client
-      @body_stat = BodyStat.new
-      @workout_plan = WorkoutPlan.new
-      render "clients/show", status: :unprocessable_entity
-    end
-  end
 
   private
-
-  def copy_session(session_id)
-    original_session = WorkoutSession.find(session_id)
-    copied_session = original_session.deep_clone include: :session_exercises
-    # have strong params from the form to update the values of copied_session
-    copied_session.assign_attributes(workout_session_params)
-    copied_session
-  end
 
   def set_workout_session
     @workout_session = WorkoutSession.find(params[:id])
