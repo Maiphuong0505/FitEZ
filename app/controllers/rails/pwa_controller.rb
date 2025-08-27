83c21ca66913504e1412b24f92b937db190b202c
MaiPhuong NguyenThi <nguyenthimaiphuong2416@gmail.com>
2025-08-27 22:45:52 +0900

    Tried to set up PWA but got lost mid-way

---
+++ app/controllers/rails/pwa_controller.rb
@@ -0,0 +1,11 @@
+class Rails::PwaController < Rails::ApplicationController
+  skip_forgery_protection
+
+  def service_worker
+    render template: "pwa/service-worker", layout: false
+  end
+
+  def manifest
+    render template: "pwa/manifest", layout: false
+  end
+end
