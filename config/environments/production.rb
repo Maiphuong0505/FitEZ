405b1b482b2174dbc8c9cac661c991befb964ceb
MaiPhuong NguyenThi <nguyenthimaiphuong2416@gmail.com>
2025-09-20 17:37:11 +0900

    Half way through changing the naming of variables and components in stimulus controller to make reusable across different models

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  Rails.application.routes.default_url_options[:host] = 'fitez.fit'
+  Rails.application.routes.default_url_options[:host] = 'www.fitez.fit'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
